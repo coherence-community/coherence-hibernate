2d70e9fbc3fc2f3dff46da7d8ab0a2d12e72b1ea
Randy Stafford <randy.stafford@oracle.com>
2013-07-23 09:33:05 -0600

    Added license headers to xml files.

--- coherence-hibernate-second-level-cache-tests/src/main/java/org/hibernate/tutorial/domain/Person.java
+++ coherence-hibernate-second-level-cache-tests/src/main/java/org/hibernate/tutorial/domain/Person.java
@@ -1,10 +1,36 @@
+/*
+ * File: Person.java
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * The contents of this file are subject to the terms and conditions of
+ * the Common Development and Distribution License 1.0 (the "License").
+ *
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the License by consulting the LICENSE.txt file
+ * distributed with this file, or by consulting https://oss.oracle.com/licenses/CDDL
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file LICENSE.txt.
+ *
+ * MODIFICATIONS:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ */
+
 package org.hibernate.tutorial.domain;
 
 import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Source copied from http://docs.jboss.org/hibernate/orm/4.2/manual/en-US/html/ch01.html.
+ * Source copied from http://docs.jboss.org/hibernate/orm/4.2/manual/en-US/html/ch01.html
+ * and adapted for functional testing of coherence-hibernate-second-level-cache.
  */
 public class Person {
 
