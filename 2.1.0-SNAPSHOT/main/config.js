b0c9a548f0432ba3b16480394815b7908917dfbd
Gunnar Hillert <gunnar.hillert@oracle.com>
2021-11-12 14:02:32 -1000

    2.1.0-SNAPSHOT Site

---
+++ 2.1.0-SNAPSHOT/main/config.js
@@ -0,0 +1,235 @@
+function createConfig() {
+    return {
+        home: "about/01_overview",
+        release: "2.1.0-SNAPSHOT",
+        releases: [
+            "2.1.0-SNAPSHOT"
+        ],
+        pathColors: {
+            "*": "blue-grey"
+        },
+        theme: {
+            primary: '#1976D2',
+            secondary: '#424242',
+            accent: '#82B1FF',
+            error: '#FF5252',
+            info: '#2196F3',
+            success: '#4CAF50',
+            warning: '#FFC107'
+        },
+        navTitle: 'Oracle Coherence Hibernate',
+        navIcon: null,
+        navLogo: 'images/logo.png'
+    };
+}
+
+function createRoutes(){
+    return [
+        {
+            path: '/about/01_overview',
+            meta: {
+                h1: 'Overview',
+                title: 'Overview',
+                h1Prefix: null,
+                description: 'Oracle Coherence Hibernate Website',
+                keywords: 'coherence, hibernate, java, documentation',
+                customLayout: null,
+                hasNav: true
+            },
+            component: loadPage('about-01_overview', '/about/01_overview', {})
+        },
+        {
+            path: '/about/02_hibernate-cache',
+            meta: {
+                h1: 'Hibernate Cache',
+                title: 'Hibernate Cache',
+                h1Prefix: null,
+                description: null,
+                keywords: null,
+                customLayout: null,
+                hasNav: true
+            },
+            component: loadPage('about-02_hibernate-cache', '/about/02_hibernate-cache', {})
+        },
+        {
+            path: '/about/03_hibernate-cache-store',
+            meta: {
+                h1: 'Coherence Hibernate CacheStore',
+                title: 'Coherence Hibernate CacheStore',
+                h1Prefix: null,
+                description: null,
+                keywords: null,
+                customLayout: null,
+                hasNav: true
+            },
+            component: loadPage('about-03_hibernate-cache-store', '/about/03_hibernate-cache-store', {})
+        },
+        {
+            path: '/dev/01_license',
+            meta: {
+                h1: 'License',
+                title: 'License',
+                h1Prefix: null,
+                description: 'Oracle Coherence Hibernate License',
+                keywords: 'coherence, hibernate, license',
+                customLayout: null,
+                hasNav: true
+            },
+            component: loadPage('dev-01_license', '/dev/01_license', {})
+        },
+        {
+            path: '/dev/02_source-code',
+            meta: {
+                h1: 'Source Code',
+                title: 'Source Code',
+                h1Prefix: null,
+                description: 'Oracle Coherence Hibernate Website',
+                keywords: 'coherence, hibernate, java, documentation',
+                customLayout: null,
+                hasNav: true
+            },
+            component: loadPage('dev-02_source-code', '/dev/02_source-code', {})
+        },
+        {
+            path: '/dev/03_build-instructions',
+            meta: {
+                h1: 'Building',
+                title: 'Building',
+                h1Prefix: null,
+                description: 'Oracle Coherence Hibernate Website',
+                keywords: 'coherence, hibernate, java, documentation',
+                customLayout: null,
+                hasNav: true
+            },
+            component: loadPage('dev-03_build-instructions', '/dev/03_build-instructions', {})
+        },
+        {
+            path: '/dev/04_issue-tracking',
+            meta: {
+                h1: 'Issue Tracking',
+                title: 'Issue Tracking',
+                h1Prefix: null,
+                description: null,
+                keywords: null,
+                customLayout: null,
+                hasNav: true
+            },
+            component: loadPage('dev-04_issue-tracking', '/dev/04_issue-tracking', {})
+        },
+        {
+            path: '/dev/05_contributions',
+            meta: {
+                h1: 'Contributing',
+                title: 'Contributing',
+                h1Prefix: null,
+                description: 'Oracle Coherence Hibernate Website',
+                keywords: 'coherence, hibernate, java, documentation',
+                customLayout: null,
+                hasNav: true
+            },
+            component: loadPage('dev-05_contributions', '/dev/05_contributions', {})
+        },
+        {
+            path: '/dev/06_history',
+            meta: {
+                h1: 'Change History',
+                title: 'Change History',
+                h1Prefix: null,
+                description: 'Oracle Coherence Hibernate Website',
+                keywords: 'coherence, hibernate, java, documentation',
+                customLayout: null,
+                hasNav: true
+            },
+            component: loadPage('dev-06_history', '/dev/06_history', {})
+        },
+        {
+            path: '/dev/07_getting-help',
+            meta: {
+                h1: 'Getting Help',
+                title: 'Getting Help',
+                h1Prefix: null,
+                description: 'Oracle Coherence Hibernate Website',
+                keywords: 'coherence, hibernate, java, documentation',
+                customLayout: null,
+                hasNav: true
+            },
+            component: loadPage('dev-07_getting-help', '/dev/07_getting-help', {})
+        },
+        {
+            path: '/', redirect: '/about/01_overview'
+        },
+        {
+            path: '*', redirect: '/'
+        }
+    ];
+}
+
+function createNav(){
+    return [
+        { header: 'Project Website' },
+        {
+            title: 'Getting Started',
+            action: 'assistant',
+            group: '/about',
+            items: [
+                { href: '/about/01_overview', title: 'Overview' },
+                { href: '/about/02_hibernate-cache', title: 'Hibernate Cache' },
+                { href: '/about/03_hibernate-cache-store', title: 'Coherence Hibernate CacheStore' }
+            ]
+        },
+        {
+            title: 'Development',
+            action: 'fa-code',
+            group: '/dev',
+            items: [
+                { href: '/dev/01_license', title: 'License' },
+                { href: '/dev/02_source-code', title: 'Source Code' },
+                { href: '/dev/03_build-instructions', title: 'Building' },
+                { href: '/dev/04_issue-tracking', title: 'Issue Tracking' },
+                { href: '/dev/05_contributions', title: 'Contributing' },
+                { href: '/dev/06_history', title: 'Change History' },
+                { href: '/dev/07_getting-help', title: 'Getting Help' }
+            ]
+        },
+        { divider: true },
+        { header: 'Reference Documentation' },
+        {
+            title: 'Javadocs',
+            action: 'code',
+            href: 'refdocs/api/index.html',
+            target: '_blank'
+        },
+        { divider: true },
+        { header: 'Additional Resources' },
+        {
+            title: 'Slack',
+            action: 'fa-slack',
+            href: 'https://join.slack.com/t/oraclecoherence/shared_invite/enQtNzcxNTQwMTAzNjE4LTJkZWI5ZDkzNGEzOTllZDgwZDU3NGM2YjY5YWYwMzM3ODdkNTU2NmNmNDFhOWIxMDZlNjg2MzE3NmMxZWMxMWE',
+            target: '_blank'
+        },
+        {
+            title: 'Coherence Web Site',
+            action: 'fa-globe',
+            href: 'https://coherence.community/',
+            target: '_blank'
+        },
+        {
+            title: 'Coherence Spring',
+            action: 'fa-globe',
+            href: 'https://spring.coherence.community/',
+            target: '_blank'
+        },
+        {
+            title: 'GitHub',
+            action: 'fa-github-square',
+            href: 'https://github.com/coherence-community/coherence-hibernate/',
+            target: '_blank'
+        },
+        {
+            title: 'Twitter',
+            action: 'fa-twitter-square',
+            href: 'https://twitter.com/OracleCoherence/',
+            target: '_blank'
+        }
+    ];
+}
---
+++ 2.1.0-SNAPSHOT/pages/about/01_overview.js
@@ -0,0 +1,131 @@
+<doc-view>
+
+<h2 id="_overview">Overview</h2>
+<div class="section">
+<p>The <a id="" title="" target="_blank" href="https://github.com/coherence-community/coherence-hibernate">Oracle Coherence Hibernate project</a>
+encompasses the code of integration points between <a id="" title="" target="_blank" href="http://hibernate.org/">Hibernate</a>
+and <a id="" title="" target="_blank" href="https://coherence.community/">Oracle Coherence</a>. Currently, the set of integration points consists of the following
+modules:</p>
+
+<ul class="ulist">
+<li>
+<p>A Hibernate-based implementation of the Coherence CacheStore SPI</p>
+
+</li>
+<li>
+<p>A Coherence-based implementation of the Hibernate second-level cache SPI</p>
+
+</li>
+</ul>
+<p>Complete source code together with documentation is provided via the GitHub repository at <a id="" title="" target="_blank" href="https://github.com/coherence-community/coherence-hibernate">https://github.com/coherence-community/coherence-hibernate</a>.</p>
+
+</div>
+
+<h2 id="_get_started">Get Started</h2>
+<div class="section">
+<v-layout row wrap class="mb-5">
+<v-flex xs12>
+<v-container fluid grid-list-md class="pa-0">
+<v-layout row wrap class="pillars">
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/about/02_hibernate-cache"><div class="card__link-hover"/>
+</router-link>
+<v-layout align-center justify-center class="">
+<v-avatar size="150px">
+<v-icon class="xxx-large">power_settings_new</v-icon>
+</v-avatar>
+</v-layout>
+<div class="px-3">
+<v-divider class="indigo lighten-4"/>
+</div>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Hibernate Cache</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Hibernate Second Level Cache SPI</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="#about/02_hibernate-cache-store.adoc" @click.native="this.scrollFix('#about/02_hibernate-cache-store.adoc')"><div class="card__link-hover"/>
+</router-link>
+<v-layout align-center justify-center class="">
+<v-avatar size="150px">
+<v-icon class="xxx-large">arrow_circle_up</v-icon>
+</v-avatar>
+</v-layout>
+<div class="px-3">
+<v-divider class="indigo lighten-4"/>
+</div>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">CacheStore</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Hibernate as the implementation of a Coherence CacheStore</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<a id="" title="" target="_blank" href="https://github.com/coherence-community/coherence-hibernate/tree/master/samples"><div class="card__link-hover"/>
+</a>
+<v-layout align-center justify-center class="">
+<v-avatar size="150px">
+<v-icon class="xxx-large">format_list_bulleted</v-icon>
+</v-avatar>
+</v-layout>
+<div class="px-3">
+<v-divider class="indigo lighten-4"/>
+</div>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Samples</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Sample Applications.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+</v-layout>
+</v-container>
+</v-flex>
+</v-layout>
+</div>
+
+<h2 id="_reference_documentation">Reference Documentation</h2>
+<div class="section">
+<v-layout row wrap class="mb-5">
+<v-flex xs12>
+<v-container fluid grid-list-md class="pa-0">
+<v-layout row wrap class="pillars">
+<v-flex xs12 sm4 lg3>
+<v-card>
+<a id="" title="" target="_blank" href="refdocs/api/index.html"><div class="card__link-hover"/>
+</a>
+<v-layout align-center justify-center class="">
+<v-avatar size="150px">
+<v-icon class="xxx-large">code</v-icon>
+</v-avatar>
+</v-layout>
+<div class="px-3">
+<v-divider class="indigo lighten-4"/>
+</div>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Javadocs</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Browse the Coherence Hibernate JavaDocs.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+</v-layout>
+</v-container>
+</v-flex>
+</v-layout>
+</div>
+</doc-view>
