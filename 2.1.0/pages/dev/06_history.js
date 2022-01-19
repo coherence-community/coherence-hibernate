26721684be9cd8b7f0d2fccd4c974a5ec2b13243
Gunnar Hillert <gunnar.hillert@oracle.com>
2022-01-19 00:06:59 -1000

    2.1.0 Site

---
+++ 2.1.0/pages/dev/06_history.js
@@ -0,0 +1,190 @@
+<doc-view>
+
+<h2 id="_change_history">Change History</h2>
+<div class="section">
+<p>The following sections outline Oracle Coherence Hibernate Integration changes in
+reverse chronological order.</p>
+
+
+<h3 id="_version_2_1_0_m1_nov_2021">Version 2.1.0-M1 (Nov 2021)</h3>
+<div class="section">
+
+<h4 id="_global_and_cross_module_changes">Global and Cross-Module Changes</h4>
+<div class="section">
+<ul class="ulist">
+<li>
+<p>Add sample application</p>
+
+</li>
+<li>
+<p>Require <code>Java 11</code> to build the project. When solely using, Java 8 is still supported</p>
+
+</li>
+</ul>
+</div>
+
+<h4 id="_coherence_hibernate_second_level_cache">coherence-hibernate-second-level-cache</h4>
+<div class="section">
+<ul class="ulist">
+<li>
+<p>Add support for configurable CacheKeysFactories in Hibernate <code>5.2.x</code></p>
+
+</li>
+<li>
+<p>Add support for Hibernate <code>5.3.x</code>, <code>5.4.x</code>, <code>5.5.x</code>, <code>5.6.x</code></p>
+
+</li>
+</ul>
+</div>
+
+<h4 id="_coherence_hibernate_site">coherence-hibernate-site</h4>
+<div class="section">
+<ul class="ulist">
+<li>
+<p>Migrate to new website format</p>
+
+</li>
+<li>
+<p>Publish JavaDoc</p>
+
+</li>
+</ul>
+</div>
+</div>
+
+<h3 id="_version_2_0_0_2020_11_25_1530">Version 2.0.0 @ 2020-11-25 15:30</h3>
+<div class="section">
+
+<h4 id="_global_and_cross_module_changes_2">Global and Cross-Module Changes</h4>
+<div class="section">
+<ul class="ulist">
+<li>
+<p>Change licensing from the COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL)
+to The Universal Permissive License (UPL)</p>
+
+</li>
+<li>
+<p>Updating of dependencies.</p>
+
+</li>
+<li>
+<p>Merge functional tests into the respective versions</p>
+
+</li>
+</ul>
+</div>
+
+<h4 id="_coherence_hibernate_cache_store">coherence-hibernate-cache-store</h4>
+<div class="section">
+<ul class="ulist">
+<li>
+<p>Update dependencies</p>
+
+</li>
+</ul>
+</div>
+
+<h4 id="_coherence_hibernate_second_level_cache_2">coherence-hibernate-second-level-cache</h4>
+<div class="section">
+<ul class="ulist">
+<li>
+<p>Add support for Hibernate <code>4.3.x</code>, <code>5.0.x</code>, <code>5.1.x</code>, <code>5.2.x</code></p>
+
+</li>
+<li>
+<p>Add additional modules for the various Hibernate versions</p>
+
+</li>
+</ul>
+</div>
+
+<h4 id="_coherence_hibernate_site_2">coherence-hibernate-site</h4>
+<div class="section">
+<ul class="ulist">
+<li>
+<p>Polishing and updating for <code>2.0.0</code></p>
+
+</li>
+<li>
+<p>Update site code</p>
+
+</li>
+</ul>
+</div>
+</div>
+
+<h3 id="_version_1_0_0_2013_09_12_1530">Version 1.0.0 @ 2013-09-12 15:30</h3>
+<div class="section">
+
+<h4 id="_source_and_documentation_contributors">Source and Documentation Contributors</h4>
+<div class="section">
+<ul class="ulist">
+<li>
+<p>Randy Stafford</p>
+
+</li>
+</ul>
+</div>
+
+<h4 id="_global_and_cross_module_changes_3">Global and Cross-Module Changes</h4>
+<div class="section">
+<ul class="ulist">
+<li>
+<p>Initial release.</p>
+
+</li>
+</ul>
+</div>
+
+<h4 id="_coherence_hibernate_cache_store_2">coherence-hibernate-cache-store</h4>
+<div class="section">
+<ul class="ulist">
+<li>
+<p>Initial release.</p>
+
+</li>
+</ul>
+</div>
+
+<h4 id="_coherence_hibernate_cache_store_tests">coherence-hibernate-cache-store-tests</h4>
+<div class="section">
+<ul class="ulist">
+<li>
+<p>Initial release.</p>
+
+</li>
+</ul>
+</div>
+
+<h4 id="_coherence_hibernate_second_level_cache_3">coherence-hibernate-second-level-cache</h4>
+<div class="section">
+<ul class="ulist">
+<li>
+<p>Initial release.</p>
+
+</li>
+</ul>
+</div>
+
+<h4 id="_coherence_hibernate_second_level_cache_tests">coherence-hibernate-second-level-cache-tests</h4>
+<div class="section">
+<ul class="ulist">
+<li>
+<p>Initial release.</p>
+
+</li>
+</ul>
+</div>
+
+<h4 id="_coherence_hibernate_site_3">coherence-hibernate-site</h4>
+<div class="section">
+<ul class="ulist">
+<li>
+<p>Initial release.</p>
+
+</li>
+</ul>
+</div>
+</div>
+</div>
+</doc-view>
