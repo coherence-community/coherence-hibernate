535b406be64b842e6ff82dfa15d37b33b9178297
GitHub Action <github-action@users.noreply.github.com>
2022-08-26 06:45:07 +0000

    2.3.1-SNAPSHOT Site

---
+++ 2.3.1-SNAPSHOT/pages/about/01_overview.js
@@ -0,0 +1,119 @@
+<doc-view>
+
+<h2 id="_overview">Overview</h2>
+<div class="section">
+<p>The <a id="" title="" target="_blank" href="https://github.com/coherence-community/coherence-hibernate">Oracle Coherence Hibernate project</a>
+encompasses the code of integration points between <a id="" title="" target="_blank" href="http://hibernate.org/">Hibernate</a>
+and <a id="" title="" target="_blank" href="https://coherence.community/">Oracle Coherence</a>. Currently, the set of integration points consists of the following
+modules:</p>
+
+<ul class="ulist">
+<li>
+<p>A Hibernate-based implementation of the <strong>Coherence CacheStore SPI</strong></p>
+
+</li>
+<li>
+<p>A Coherence-based implementation of the <strong>Hibernate second-level cache SPI</strong></p>
+
+</li>
+</ul>
+<p>Complete source code together with documentation is provided via the GitHub repository at <a id="" title="" target="_blank" href="https://github.com/coherence-community/coherence-hibernate">https://github.com/coherence-community/coherence-hibernate</a>.</p>
+
+</div>
+
+<h2 id="_documentation">Documentation</h2>
+<div class="section">
+<v-layout row wrap class="mb-5">
+<v-flex xs12>
+<v-container fluid grid-list-md class="pa-0">
+<v-layout row wrap class="pillars">
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/about/02_hibernate-cache"><div class="card__link-hover"/>
+</router-link>
+<v-layout align-center justify-center class="">
+<v-avatar size="150px">
+<v-icon class="xxx-large">cached</v-icon>
+</v-avatar>
+</v-layout>
+<div class="px-3">
+<v-divider class="indigo lighten-4"/>
+</div>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Hibernate Cache</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Hibernate Second Level Cache SPI</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<router-link to="/about/03_hibernate-cache-store"><div class="card__link-hover"/>
+</router-link>
+<v-layout align-center justify-center class="">
+<v-avatar size="150px">
+<v-icon class="xxx-large">arrow_circle_up</v-icon>
+</v-avatar>
+</v-layout>
+<div class="px-3">
+<v-divider class="indigo lighten-4"/>
+</div>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">CacheStore</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Hibernate as the implementation of a Coherence CacheStore</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<a id="" title="" target="_blank" href="https://github.com/coherence-community/coherence-hibernate/tree/master/samples"><div class="card__link-hover"/>
+</a>
+<v-layout align-center justify-center class="">
+<v-avatar size="150px">
+<v-icon class="xxx-large">format_list_bulleted</v-icon>
+</v-avatar>
+</v-layout>
+<div class="px-3">
+<v-divider class="indigo lighten-4"/>
+</div>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Samples</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Sample Applications.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+<v-flex xs12 sm4 lg3>
+<v-card>
+<a id="" title="" target="_blank" href="api/index.html"><div class="card__link-hover"/>
+</a>
+<v-layout align-center justify-center class="">
+<v-avatar size="150px">
+<v-icon class="xxx-large">code</v-icon>
+</v-avatar>
+</v-layout>
+<div class="px-3">
+<v-divider class="indigo lighten-4"/>
+</div>
+<v-card-title primary class="headline layout justify-center">
+<span style="text-align:center">Javadocs</span>
+</v-card-title>
+<v-card-text class="caption">
+<p></p>
+<p>Browse the Coherence Hibernate JavaDocs.</p>
+</v-card-text>
+</v-card>
+</v-flex>
+</v-layout>
+</v-container>
+</v-flex>
+</v-layout>
+</div>
+</doc-view>
---
+++ 2.3.1-SNAPSHOT/pages/dev/05_contributions.js
@@ -0,0 +1,23 @@
+<doc-view>
+
+<h2 id="_contributing">Contributing</h2>
+<div class="section">
+<p>The following sections outline the requirements for making contributions to the
+Coherence Hibernate project.</p>
+
+
+<h3 id="_overview">Overview</h3>
+<div class="section">
+<p>We would love your contributions to the Coherence Hibernate project!</p>
+
+</div>
+
+<h3 id="_becoming_a_contributor">Becoming a Contributor</h3>
+<div class="section">
+<p>The details of how to contribute to Coherence Hibernate are detailed in
+<a id="" title="" target="_blank" href="https://github.com/coherence-community/coherence-hibernate/blob/master/CONTRIBUTING.adoc">CONTRIBUTING.adoc</a>
+in the Coherence Hibernate GitHub repository.</p>
+
+</div>
+</div>
+</doc-view>
