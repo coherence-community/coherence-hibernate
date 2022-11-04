299a918cca3768ae708e6a4c6652649c715cecb5
GitHub Action <github-action@users.noreply.github.com>
2022-11-04 08:12:57 +0000

    2.3.2-SNAPSHOT Site

---
+++ 2.3.2-SNAPSHOT/pages/dev/02_source-code.js
@@ -0,0 +1,22 @@
+<doc-view>
+
+<h2 id="_source_code">Source Code</h2>
+<div class="section">
+<p>The Source Code for Coherence Hibernate is hosted on <a id="" title="" target="_blank" href="https://github.com">github.com</a>.</p>
+
+<p>It is located here:
+<a id="" title="" target="_blank" href="https://github.com/coherence-community/coherence-hibernate">https://github.com/coherence-community/coherence-hibernate</a></p>
+
+<p>Alternatively, if you&#8217;re not using or want to avoid setting up <a id="" title="" target="_blank" href="http://git-scm.com">git</a>, you
+can download a zip containing the latest development branch from here:</p>
+
+<p><a id="" title="" target="_blank" href="https://github.com/coherence-community/coherence-hibernate/archive/master.zip">https://github.com/coherence-community/coherence-hibernate/archive/master.zip</a></p>
+
+<div class="admonition important">
+<p class="admonition-textlabel">Important</p>
+<p ><p><strong>Pull Requests</strong> are welcome! Just make sure to become an official
+<a id="" title="" target="_blank" href="https://github.com/coherence-community/coherence-hibernate/blob/master/CONTRIBUTING.adoc">contributor</a>.</p>
+</p>
+</div>
+</div>
+</doc-view>
